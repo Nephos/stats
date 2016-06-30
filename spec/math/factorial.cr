fcb2223a0188729b760a21a2e8486fbd91254c97
Arthur Poulet <arthur.poulet@mailoo.org>
2016-06-30 20:54:40 +0200

    improve factorial to handle BitInt

--- spec/math/factorial.cr
+++ spec/math/factorial.cr
@@ -8,6 +8,12 @@ describe Math do
     Math.factorial(4).should eq 24
     Math.factorial(5).should eq 120
     Math.factorial(6).should eq 720
+    Math.factorial(6).should eq 720
+  end
+
+  it "factorial bigint" do
+    res = (1..20).to_a.reduce(BigInt.new 1) {|e, i| e * BigInt.new(i) }
+    Math.factorial(BigInt.new 20).should eq res
   end
 
 end
--- src/stats/math/factorial.cr
+++ src/stats/math/factorial.cr
@@ -1,9 +1,11 @@
 module Math
+  module Factorial(U)
 
-  def self.factorial(n : Int::All)
-    raise Math::DomainError.new "The argument must be a natural (out of domain -- factorial)" if n < 0
-    return 1 if n == 0
-    return (1..n).to_a.reduce{|a, b| a * b} #(:*)
+    def factorial(n : U)
+      raise Math::DomainError.new "The argument must be a natural (out of domain -- factorial)" if n < 0
+      return U.new(1) if n == 0
+      return (1..n).to_a.reduce(U.new(1)){|a, b| a * b}
+    end
+
   end
-
 end
